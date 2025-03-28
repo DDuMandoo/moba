0fbe19af8245d51ea28fa98c680603d45a154e94
오준수 <ojs9551@naver.com>
2025-03-28 17:34:19 +0900

    fix: 회원가입 api를 두 개로 분리 및 record와 빌더로 수정

--- back-end/moba/src/main/java/com/a601/moba/global/code/ErrorCode.java
+++ back-end/moba/src/main/java/com/a601/moba/global/code/ErrorCode.java
@@ -56,9 +56,9 @@ public enum ErrorCode {
     APPOINTMENT_PARTICIPANT_LIMIT_EXCEEDED(4409, BAD_REQUEST, "약속방 참여 인원 제한을 초과하였습니다"),
     APPOINTMENT_EXIT_FORBIDDEN(4410, FORBIDDEN, "방장은 약속방을 나갈 수 없습니다"),
 
-    // ================= S3 관련 에러 (4500번대) ==================
-    S3_UPLOAD_FAILED(4500, INTERNAL_SERVER_ERROR, "S3 파일 업로드에 실패했습니다"),
-    S3_DELETE_FAILED(4501, INTERNAL_SERVER_ERROR, "S3 파일 삭제에 실패했습니다"),
+    // ================= S3 관련 에러 (4600번대) ==================
+    S3_UPLOAD_FAILED(4600, INTERNAL_SERVER_ERROR, "S3 파일 업로드에 실패했습니다"),
+    S3_DELETE_FAILED(4601, INTERNAL_SERVER_ERROR, "S3 파일 삭제에 실패했습니다"),
 
     ;
 
